11f43d6fc18d60b53f5dc5dadb1d4b7b424d82b7
thanh.ph <phamhoaithanh32@gmail.com>
2025-03-05 11:06:09 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 export default function Home() {
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <div
+                    onClick={() => {
+                        window.location.href = 'https://concung.com/sua-bot/spdd-c-thuc-icreo-learning-milk-820g-70077.html';
+                    }}
                     className={"w-[300px] h-[60px] bg-white rounded-[8px] text-black flex items-center justify-center font-bold"}>
                     Redirect
                 </div>
