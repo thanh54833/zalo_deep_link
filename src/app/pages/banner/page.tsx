d78c1c1e2a3d2c76e3826b87d0fb48ddb84bab48
thanh.ph <phamhoaithanh32@gmail.com>
2025-03-06 13:10:02 +0700

    update

--- src/app/pages/banner/page.tsx
+++ src/app/pages/banner/page.tsx
@@ -1,11 +1,27 @@
 'use client';
 import Image from 'next/image';
+import {useEffect} from "react";
 
 export default function Banner() {
+
+    const url_redirect_fallback = "deeplink://sua-bot/spdd-c-thuc-icreo-learning-milk-820g-70077.html";
+    // const url_redirect = 'https://concung.com/sua-bot/spdd-c-thuc-icreo-learning-milk-820g-70077.html'
+
+    useEffect(() => {
+        // if (navigator.userAgent.match(/(Zalo)/)) {
+        //     window.location.href = url_redirect
+        // }
+        const timer = setTimeout(() => {
+            window.location.href = url_redirect_fallback
+        }, 100);
+        return () => clearTimeout(timer);
+    }, []);
+
+
     return (
         <div className="h-screen flex flex-col bg-white flex-1">
             <div className="w-screen bg-white p-[6px] flex flex-row">
-                <Image src="/img.png" alt="Image" width={40} height={40} className="w-[50px] h-[50px]" />
+                <Image src="/img.png" alt="Image" width={40} height={40} className="w-[50px] h-[50px]"/>
                 <div className="flex flex-row flex-1 content-center items-center">
                     <div className="flex flex-col flex-1 ml-[10px]">
                         <div className="font-bold">
@@ -16,9 +32,9 @@ export default function Banner() {
                         </div>
                     </div>
                     <div
-                       onClick={() => {
-                           window.location.href = "https://apps.apple.com/vn/app/con-c%C6%B0ng-1-m%E1%BA%B9-v%C3%A0-b%C3%A9/id1442035575?l=vi";
-                       }}
+                        onClick={() => {
+                            window.location.href = "https://apps.apple.com/vn/app/con-c%C6%B0ng-1-m%E1%BA%B9-v%C3%A0-b%C3%A9/id1442035575?l=vi";
+                        }}
                         className="bg-pink-400 h-[30px] text-white px-[10px] rounded-[15px] font-bold ml-auto align-middle text-[10px] items-center content-center"
                     >
                         INSTALL
