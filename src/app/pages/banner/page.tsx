cd80babf083bbafbb5ba0f09e47b5a19592aa6f9
thanh.ph <phamhoaithanh32@gmail.com>
2025-03-06 11:41:19 +0700

    update

---
+++ src/app/pages/banner/page.tsx
@@ -0,0 +1,14 @@
+export default function Banner() {
+    return (
+        <div className="h-full flex flex-col bg-white flex-1">
+            <div className="h-[50px] w-[50px] bg-white absolute"></div>
+            <div id="iframe" className="h-full w-full flex-1">
+                <iframe
+                    src="/sua-bot_spdd-c-thuc-icreo-learning-milk-820g-70077.html"
+                    className="w-full h-full"
+                    frameBorder="0"
+                />
+            </div>
+        </div>
+    );
+}
