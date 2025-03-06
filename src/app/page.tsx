6db807df430f786edf4ff8418481f0b5e67d797f
thanh.ph <phamhoaithanh32@gmail.com>
2025-03-06 11:54:34 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,22 +18,11 @@ export default function Home() {
 
     return (
         <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black"
-        >
-            <head>
-                <meta name="apple-itunes-app" content="app-id=1442035575"/>
-                <meta name="google-play-app" content="app-id=com.example.android"/>
-            </head>
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <div
-                    id={"redirect_id"}
-                    onClick={() => {
-                        window.location.href = url_redirect
-                    }}
-                    className={"w-[300px] h-[60px] bg-white rounded-[8px] text-black flex items-center justify-center font-bold"}>
-                    Redirect
-                </div>
-            </main>
+            onClick={() => {
+                window.location.href = url_redirect
+            }}
+            className={"w-[300px] h-[60px] bg-white rounded-[8px] text-black flex items-center justify-center font-bold"}>
+            Redirect
         </div>
     );
 }
--- src/app/pages/banner/page.tsx
+++ src/app/pages/banner/page.tsx
@@ -4,7 +4,7 @@ export default function Banner() {
             <div className="h-[50px] w-[50px] bg-white absolute"></div>
             <div id="iframe" className="h-full w-full flex-1">
                 <iframe
-                    src="/sua-bot_spdd-c-thuc-icreo-learning-milk-820g-70077.html"
+                    src="http://10.10.11.88:3000/sua-bot_spdd-c-thuc-icreo-learning-milk-820g-70077.html"
                     className="w-full h-full"
                     frameBorder="0"
                 />
