31a57f127083b5f46cf5c22041662017e1942ccf
thanh.ph <phamhoaithanh32@gmail.com>
2025-03-07 14:17:21 +0700

    update

--- src/app/pages/shortlink/page.tsx
+++ src/app/pages/shortlink/page.tsx
@@ -35,7 +35,7 @@ const Banner: React.FC = () => {
     }, [url]);
 
     const get_url_fallback = (url_cookie: string): string => {
-        return url_cookie ? custom_url_schema + url_cookie.replace("https://concung.com/", "") : "";
+        return url_cookie ? custom_url_schema + url_cookie.replace("https://", "") : "";
     };
 
     const addUrl = () => {
@@ -80,6 +80,25 @@ const Banner: React.FC = () => {
                     >
                         Enter
                     </button>
+
+                    {/*<button*/}
+                    {/*    onClick={() => {*/}
+                    {/*        window.location.href = "deeplink://reactPath?navigate_url=https://concung.com/sua-bot/spdd-cong-thuc-colosbaby-gold-d3k2-2-800g-s-69977.html";*/}
+                    {/*    }}*/}
+                    {/*    className="w-24 bg-blue-500 text-white px-4 py-2 rounded"*/}
+                    {/*>*/}
+                    {/*    Link 1*/}
+                    {/*</button>*/}
+
+                    {/*<button*/}
+                    {/*    onClick={() => {*/}
+                    {/*        //window.location.href = "deeplink://concung.com/bim-ta-khuyen-mai/ta-quan-merries-ultra-jumbo-xxl-32-mieng-64881.html";*/}
+                    {/*        window.location.href = "shopeevn://reactPath?navigate_url=https%3A%2F%2Fshopee.vn%2F%3Fc%3Dfreeshipping%26pid%3Dmbb%26smtt%3D9%26utm_medium%3Dmobile%26utm_source%3Dbrowser&path=shopee%2FTRANSFER_PAGE&tab=buy&utm_medium=mobile&utm_source=browser";*/}
+                    {/*    }}*/}
+                    {/*    className="w-24 bg-blue-500 text-white px-4 py-2 rounded"*/}
+                    {/*>*/}
+                    {/*    Link 2*/}
+                    {/*</button>*/}
                 </div>
             </div>
         </div>
