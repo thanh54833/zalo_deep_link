3d4cca45551add43bfe4ea9adbbdbd83f5cdbba4
thanh.ph <phamhoaithanh32@gmail.com>
2025-03-20 11:54:52 +0700

    update

--- src/app/pages/shortlink/page.tsx
+++ src/app/pages/shortlink/page.tsx
@@ -3,50 +3,29 @@ import Image from 'next/image';
 import {useEffect, useState} from 'react';
 
 const Banner: React.FC = () => {
-    const [url, setUrl] = useState<string>('');
-    const custom_url_schema: string = "deeplink://";
 
+    const [url, setUrl] = useState('');
+
+
+    // http://localhost:3000/pages/shortlink
     useEffect(() => {
-        const getCookie = (name: string): string | undefined => {
-            const value = `; ${document.cookie}`;
-            const parts = value.split(`; ${name}=`);
-            return parts.length === 2 ? parts.pop()?.split(';').shift() : undefined;
-        };
+        const primaryUrl = "https://concung.com/sua-tuoi-cac-loai/sua-tuoi-tiet-trung-co-duong-vinamilk-180ml-loc-4-hop-44928.html";
+        const fallbackUrl = "deeplink://" + primaryUrl.replace("https://", "");
 
-        const url_cookie = getCookie('url');
 
-        if (url_cookie) {
-            setUrl(url_cookie);
+        window.location.href = primaryUrl;
+        setUrl(primaryUrl);
+
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        if (isMobile) {
+            const time = setTimeout(() => {
+                window.location.href = fallbackUrl;
+                setUrl(fallbackUrl);
+            }, 100);
+            return () => clearTimeout(time);
         }
+    }, []);
 
-
-        const timer = setTimeout(() => {
-            if (navigator.userAgent.includes('Zalo') && url_cookie) {
-                const fallbackUrl = get_url_fallback(url_cookie);
-                if (fallbackUrl) {
-                    window.location.href = fallbackUrl;
-                }
-            } else if (url) {
-                window.location.href = url;
-            }
-        }, 100);
-
-        return () => clearTimeout(timer);
-    }, [url]);
-
-    const get_url_fallback = (url_cookie: string): string => {
-
-
-        return url_cookie ? custom_url_schema + url_cookie.replace("https://", "") : "";
-    };
-
-    const addUrl = () => {
-        const urlValue = (document.getElementById('id_text') as HTMLInputElement).value.trim();
-        if (urlValue) {
-            setUrl(urlValue);
-            document.cookie = `url=${urlValue}; path=/;`;
-        }
-    };
 
     return (
         <div className="h-screen flex flex-col bg-white text-black">
@@ -66,46 +45,11 @@ const Banner: React.FC = () => {
                 </div>
             </div>
             <div className="bg-pink-200 w-screen h-px"></div>
-            <div className="flex-1 flex flex-col items-center justify-center mx-5">
-                <div><strong>url_redirect:</strong> {url}</div>
-                <div><strong>custom_url:</strong> {get_url_fallback(url)}</div>
-                <div className="mt-5 flex flex-col items-center w-full">
-                    <input
-                        type="text"
-                        id="id_text"
-                        placeholder="Enter URL"
-                        className="w-full border p-2 mb-4"
-                    />
-                    <button
-                        onClick={addUrl}
-                        className="w-24 bg-blue-500 text-white px-4 py-2 rounded"
-                    >
-                        Enter
-                    </button>
-
-                    {/*<button*/}
-                    {/*    onClick={() => {*/}
-                    {/*        window.location.href = "deeplink://reactPath?navigate_url=https://concung.com/sua-bot/spdd-cong-thuc-colosbaby-gold-d3k2-2-800g-s-69977.html";*/}
-                    {/*    }}*/}
-                    {/*    className="w-24 bg-blue-500 text-white px-4 py-2 rounded"*/}
-                    {/*>*/}
-                    {/*    Link 1*/}
-                    {/*</button>*/}
-
-                    {/*<button*/}
-                    {/*    onClick={() => {*/}
-                    {/*        //window.location.href = "deeplink://concung.com/bim-ta-khuyen-mai/ta-quan-merries-ultra-jumbo-xxl-32-mieng-64881.html";*/}
-                    {/*        window.location.href = "shopeevn://reactPath?navigate_url=https%3A%2F%2Fshopee.vn%2F%3Fc%3Dfreeshipping%26pid%3Dmbb%26smtt%3D9%26utm_medium%3Dmobile%26utm_source%3Dbrowser&path=shopee%2FTRANSFER_PAGE&tab=buy&utm_medium=mobile&utm_source=browser";*/}
-                    {/*    }}*/}
-                    {/*    className="w-24 bg-blue-500 text-white px-4 py-2 rounded"*/}
-                    {/*>*/}
-                    {/*    Link 2*/}
-                    {/*</button>*/}
-                </div>
+            <div className="flex-1 flex flex-col items-center justify-center mx-5 bg-blue-200">
+                {url}
             </div>
         </div>
     );
 };
-
 
 export default Banner;
